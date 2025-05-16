cf3815fdb9451c00696ae5b7b9f535753f464fac
AlvarVG <alvigo92@gmail.com>
2025-05-16 10:43:09 +0200

    Create READMEs for all the folders

--- infrastructure_automation/variables.tf
+++ infrastructure_automation/variables.tf
@@ -9,9 +9,6 @@ variable "observed_instance_type" {
   description = "Name of the generated ssh key"
   default     = "t3.xlarge"
 }
-# t3.micro -> Not enough memory
-# t3.large -> Not enough memory
-# t3.xlarge -> Works fine
 
 variable "observer_instance_type" {
   type        = string
