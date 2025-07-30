6eca4937a3cfb6eafd5d6bde0b1200c646d5f753
Alvar Viana Gomez <alvigo92@gmail.com>
2025-07-30 12:58:04 +0200

    Create workload files (#5)
    * Create workloads and prepare the system to run them
    * Update TODO list

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 
 In this document you will find all the different topics I will be working on. In case you want to contribute, Pull Requests are wellcome! 
 
-* Add support for MongoDB scenarios.
+* Only copy and update the files to be used
 * Improve existing dashboards.
 * Debezium Snapshotting dashboard.
 * Create workloads for specific situations (snapshotting vs streaming).
--- infrastructure_automation/variables.tf
+++ infrastructure_automation/variables.tf
@@ -19,6 +19,32 @@ variable "observer_instance_type" {
 variable "database_to_deploy" {
   type        = string
   description = "Database we are going to deploy"
-  default     = "mysql"
+  default     = "mongodb"
   # Options: mysql, postgresql or mongodb
+}
+
+variable "workload_to_run" {
+  type        = string
+  description = "Which workload are you going to run"
+  default     = "1t_1m_05i_05u_uniform"
+  # Options: Pick one from observer_machine/scenarios/workloads/
+}
+
+variable "desired_throughput" {
+  type        = string
+  description = "The troughput goal you will like to achieve"
+  default     = "100"
+}
+
+variable "num_ycsb_process" {
+  type        = string
+  description = "How many ycsb processes you want to run in parallel"
+  default     = "1"
+}
+
+variable "tables_to_use" {
+  type        = string
+  description = "Wherer "
+  default     = "usertable_0"
+  # Options: Select any combinations from usertable_0,usertable_1,usertable_2,usertable_3,usertable_4,usertable_5,usertable_6,usertable_7,usertable_8,usertable_9
 }
